2f2e1a603f156244e48d97de3cd7801c2782665b
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-05-23 00:13:24 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
-const port = process.env.PORT;
+
 const app = express();
+const port = process.env.PORT;
 
+app.use(bodyParser.json());
 
-// Server can start with "npm start"
+app.use('/auth', authRoutes);
 
+// Server can start with "npm start and will listen on port 3000"
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
