e34707445b1ed0d70ded51be2a8b600936ea24ac
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-05-26 08:56:06 +0300

    Added home-route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const homeRoutes = require('./routes/homeRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
+app.use('/', homeRoutes);
 
 // Server can start with "npm start and will listen on port 3000"
 app.listen(port, () => {
