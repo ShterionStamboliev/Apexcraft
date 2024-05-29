128c97c81f7109514f9b9da4028b8935204cb2ab
NaN-niki <niki_stst@abv.bg>
2024-05-30 01:31:04 +0300

    cors added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const homeRoutes = require('./routes/homeRoutes');
+const cors = require('cors')
+
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +13,10 @@ app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.use('/', homeRoutes);
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 // Server can start with "npm start and will listen on port 3000"
 app.listen(port, () => {
