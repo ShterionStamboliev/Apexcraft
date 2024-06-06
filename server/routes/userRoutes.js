958245f8a29071f7becd6e08a0d323e6155ff1a5
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-06-06 22:28:06 +0100

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,6 @@ const { getAssociatedUsers, userEdit } = require('../controllers/userController'
 const router = express.Router();
 
 router.get('/users', authenticateToken, getAssociatedUsers);
-router.post('/users/:id/edit', userEdit)
+
 
 module.exports = router;
