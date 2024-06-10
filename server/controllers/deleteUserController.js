3e80ba9398ee20598d94585ed6202495333da860
NaN-niki <niki_stst@abv.bg>
2024-06-10 18:35:35 +0300

    Added changes to delete logic

--- server/controllers/deleteUserController.js
+++ server/controllers/deleteUserController.js
@@ -3,7 +3,7 @@ const db = require('../db');
 const deleteUserController = async (req, res) => {
     try {
         const id = req.params.id
-        await db.execute(`DELETE FROM tbl_users WHERE id = ?`, [id])
+        await db.execute(`UPDATE tbl_users SET status = 'inactive' WHERE id = ?`, [id])
     } catch (error) {
         console.error('Error deleting user:', error.message);
         res.status(500).json({ error: 'Internal server error' });
