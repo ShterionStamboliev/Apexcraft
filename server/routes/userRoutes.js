38c6322be337caa8da5333b9f777c7a411d97d42
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-06-15 13:12:19 +0300

    Refactor folders

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const authenticateToken = require('../middlewares/authenticateToken');
-const deleteUserController = require('../controllers/deleteUserController');
-const { getAssociatedUsers } = require('../controllers/userController');
-const { editUser } = require('../controllers/editUserController');
-const { createUser } = require('../controllers/createUserController');
+const deactivateUserController = require('../controllers/user/deactivateUserController');
+const { getAssociatedUsers } = require('../controllers/user/userController');
 const getUserById = require('../utils/getUserById');
+const { createUser } = require('../controllers/user/createUserController');
+const { editUser } = require('../controllers/user/editUserController');
 
 const router = express.Router();
 
@@ -12,6 +12,6 @@ router.post('/users/create', authenticateToken, createUser);
 router.get('/users', authenticateToken, getAssociatedUsers);
 router.get('/users/:id', authenticateToken, getUserById);
 router.post('/users/:id/edit', authenticateToken, editUser);
-router.get('/users/:id/delete', authenticateToken, deleteUserController)
+router.get('/users/:id/delete', authenticateToken, deactivateUserController)
 
 module.exports = router;
