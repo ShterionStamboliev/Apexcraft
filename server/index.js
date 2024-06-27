a1b1a1f25937baf5480c5916ce3b5c513cd642c8
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-06-27 18:05:06 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,9 +15,11 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: 'https://project34-test.onrender.com',
-    credentials: true,
-    optionsSuccessStatus: 200
+    origin: 'http://localhost:5173',
+    credentials: true
+    // origin: 'https://project34-test.onrender.com',
+    // credentials: true,
+    // optionsSuccessStatus: 200
 }));
 
 app.use('/auth', authRoutes);
