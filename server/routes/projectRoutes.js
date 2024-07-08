0d6629f7230d8a94e268b984d8da01e209f0dfba
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-07-08 19:24:49 +0300

    Added get porject by ID

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -4,10 +4,12 @@ const { getAllProjects } = require('../controllers/projects/projectsController')
 const { createProject } = require('../controllers/projects/createProjectController');
 const { editProject } = require('../controllers/projects/editProjectController');
 const modifyProjectStatus = require('../controllers/projects/modifyProjectStatusController');
+const getProjectById = require('../controllers/projects/getProjectById');
 
 const router = express.Router();
 
 router.get('/projects', authenticateToken, getAllProjects);
+router.get('/projects/id', authenticateToken, getProjectById);
 router.post('/projects/create', authenticateToken, createProject );
 router.put('/projects/:id/edit', authenticateToken, editProject );
 router.put('/projects/:id/modifyStatus', authenticateToken, modifyProjectStatus)
