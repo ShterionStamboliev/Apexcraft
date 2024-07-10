c1d2fa5eaf6270b5eb4c2e9f865908752a494cf7
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-07-10 20:23:29 +0100

    Add endpoint to retrieve a measure by ID

--- server/routes/measuresRoutes.js
+++ server/routes/measuresRoutes.js
@@ -3,10 +3,12 @@ const authenticateToken = require("../middlewares/authenticateToken");
 const { editMeasure } = require("../controllers/measures/editMeasureController");
 const { addMeasure } = require("../controllers/measures/addMeasureController");
 const { getAllMeasures } = require("../controllers/measures/getAllMeasures");
+const { getMeasureById } = require("../controllers/measures/getMeasureById");
 
 const router = express.Router();
 
 router.get("/measures", authenticateToken, getAllMeasures)
+router.get("/measures/:id", authenticateToken, getMeasureById)
 router.put("/measures/:id/edit", authenticateToken, editMeasure)
 router.post("/measures/create", authenticateToken, addMeasure)
 
