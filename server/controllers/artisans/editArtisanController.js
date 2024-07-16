e1670f851995b17c3ed5d5386a97a90fcc362e9f
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-07-16 23:07:42 +0300

    Update editArtisanController.js

--- server/controllers/artisans/editArtisanController.js
+++ server/controllers/artisans/editArtisanController.js
@@ -1,74 +1,43 @@
 const db = require('../../db');
+const { getCompanyIdByName } = require('../../utils/getCompanyIdByName');
+const { getUserIdByName } = require('../../utils/getUserIdByName');
 
 const editArtisan = async (req, res) => {
 
     const userId = req.params.id;
-    const { name, note, company_id, user_id, status } = req.body;
-
-    // Constructing the query dynamically
-    let query = 'UPDATE tbl_artisans SET ';
-    let queryParams = [];
-
-    //Validate company and user
-    if (company_id === null || company_id === undefined) {
-        return res.status(400).send('Invalid or missing company');
-    }
-    if (user_id === null || user_id === undefined) {
-        return res.status(400).send('Invalid or missing user');
-    }
+    const { name, note, company, user, status } = req.body;
 
     try {
-        if (name) {
-            //Check if Artisan exists
-            const [artisanRow] = await db.execute('SELECT * FROM tbl_artisans WHERE id = ?', [userId]);
-            if (artisanRow.length === 0) {
-                return res.status(404).send('Artisan not found.');
-            }
-            query += 'name = ?, ';
-            queryParams.push(name);
-        }
-        if (note) {
-            query += 'note = ?, ';
-            queryParams.push(note);
-        }
-        if (company_id) {
-            //Check if company exists
-            if (company_id !== null && company_id !== undefined) {
-                const [companyRows] = await db.execute('SELECT 1 FROM tbl_companies WHERE id = ?', [company_id]);
-                if (companyRows.length === 0) {
-                    return res.status(404).send('Company not found!');
-                }
-            }
-            query += 'company_id = ?, ';
-            queryParams.push(company_id);
-        }
-        if (user_id) {
-            //Check if user exists
-            if (user_id !== null && user_id !== undefined) {
-                const [userRows] = await db.execute('SELECT 1 FROM tbl_users WHERE id = ?', [user_id]);
-                if (userRows.length === 0) {
-                    return res.status(404).send('User not found!');
-                }
-            }
-            query += 'user_id = ?, ';
-            queryParams.push(user_id);
-        }
-        if (status) {
-            query += 'status = ?, ';
-            queryParams.push(status);
-        }
 
-        //Remove the last comma and space
-        query = query.slice(0, -2);
-        query += ' WHERE id = ?'
-        queryParams.push(userId)
+        if (!name || !status) {
+            return res.status(400).json({ message: 'Name and Status are required fields!' });
+        };
 
+        const query = `UPDATE tbl_artisans
+        SET name = ?, note = ?, company = ?, user = ?, status = ?
+        WHERE id = ?`;
 
-        const [result] = await db.execute(query, queryParams)
-        res.status(200).send('User updated successfully')
+        const foundCompany = await getCompanyIdByName(company);
+
+        const foundUser = await getUserIdByName(user);
+
+        const values = [name, note, foundCompany, foundUser, status, userId];
+
+        const [result] = await db.execute(query, values);
+
+        const updatedArtisan = {
+            id: userId,
+            name,
+            note, 
+            foundCompany,
+            foundUser,
+            status,
+        };
+
+        res.status(201).json({ message: 'Artisan created successfully!', artisan: updatedArtisan });
+        
     } catch (error) {
-        console.error('Database error:', error);
-        res.status(500).send(error);
+        res.status(500).json({ message: 'Error creating the activity!', error });
     }
 };
 
