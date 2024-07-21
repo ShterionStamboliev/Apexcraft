1d03265bc7611a0e4a8d16589e80b1dc65215a8a
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-07-21 13:12:19 +0300

    push

--- server/validators/controllerValidator.js
+++ server/validators/controllerValidator.js
@@ -10,33 +10,21 @@ class Validator {
             let rules = this.schema[field];
 
             if (rules.required && !data[field]) {
-                errors.push(`${field} is required`);
+                errors.push(`${field} is required!`);
                 continue;
             }
 
             if (rules.type === 'string') {
                 if (typeof data[field] !== 'string') {
-                    errors.push(`${field} must be a string`);
+                    errors.push(`${field} must be a string!`);
                 }
                 if (rules.minLength && data[field].length < rules.minLength) {
-                    errors.push(`${field} must be at least ${rules.minLength} characters long`);
+                    errors.push(`${field} must be at least ${rules.minLength} characters long!`);
                 }
                 if (rules.maxLength && data[field].length > rules.maxLength) {
-                    errors.push(`${field} must be less than ${rules.maxLength} characters long`);
+                    errors.push(`${field} must be less than ${rules.maxLength} characters long!`);
                 }
             }
-
-            // if (rules.type === 'number') {
-            //     if (typeof data[field] !== 'number') {
-            //         errors.push(`${field} must be a number`);
-            //     }
-            //     if (rules.min && data[field] < rules.min) {
-            //         errors.push(`${field} must be at least ${rules.min}`);
-            //     }
-            //     if (rules.max && data[field] > rules.max) {
-            //         errors.push(`${field} must be less than ${rules.max}`);
-            //     }
-            // }
         }
 
         return errors;
