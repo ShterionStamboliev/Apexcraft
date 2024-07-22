65bfd8ff65f2c65963379b76757a173ce3a42ed7
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-07-23 00:02:04 +0300

    user edit hook refactor

--- client/src/components/hooks/UserHooks/useEditUser.ts
+++ client/src/components/hooks/UserHooks/useEditUser.ts
@@ -1,52 +1,16 @@
 import { useUser } from '@/context/User/UserContext';
-import { UserFormType } from '@/types/user-types/userTypes';
-import useToastHook from '../custom-hooks/useToastHook';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { User, UserFormType } from '@/types/user-types/userTypes';
 import { formSchema } from '@/components/models/user/editUserSchema';
+import useEditEntity from '../custom-hooks/useEditEntityHandler';
 
 const useEditUser = (user: UserFormType, onSuccess?: () => void) => {
-    const { editEntity, isLoading } = useUser();
-    const { fireToast } = useToastHook();
-
-    const form = useForm<UserFormType>({
-        defaultValues: user && {
-            ...user,
-            password: '*******'
-        },
-        resolver: zodResolver(formSchema)
+    return useEditEntity<User>({
+        entity: user,
+        initialFormState: { ...user, password: '*******' },
+        schema: formSchema,
+        useEntityContext: useUser,
+        onSuccess,
     });
-
-    const { reset } = form;
-
-    const onSubmit = async (data: UserFormType) => {
-        try {
-            if (user?.id) {
-                const isEditSuccess = await editEntity(user.id, data);
-                if (isEditSuccess && onSuccess) {
-                    onSuccess();
-                    reset();
-                    fireToast({
-                        title: 'Редакцията беше успешна',
-                        variant: 'success',
-                    });
-                }
-            }
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                fireToast({
-                    title: error.message,
-                    variant: 'destructive',
-                });
-            }
-        }
-    };
-
-    return {
-        form,
-        isLoading,
-        onSubmit
-    };
 };
 
 export default useEditUser;
