a9d5158b6530ad4550d100f185c69a7e3f2b1525
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-07-22 23:48:16 +0300

    edit project hook refactor

--- client/src/components/hooks/ProjectsHooks/useEditProject.ts
+++ client/src/components/hooks/ProjectsHooks/useEditProject.ts
@@ -1,51 +1,16 @@
-import useToastHook from '../custom-hooks/useToastHook';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Project } from '@/types/project-types/projectTypes';
 import { useProject } from '@/context/Project/ProjectContext';
 import { newProjectSchema } from '@/components/models/project/newProjectSchema';
+import useEditEntity from '../custom-hooks/useEditEntityHandler';
 
 const useEditProject = (project: Project, onSuccess?: () => void) => {
-    const { editEntity, isLoading } = useProject();
-    const { fireToast } = useToastHook();
-
-    const form = useForm<Project>({
-        defaultValues: project && {
-            ...project
-        },
-        resolver: zodResolver(newProjectSchema)
+    return useEditEntity<Project>({
+        entity: project,
+        initialFormState: { ...project },
+        schema: newProjectSchema,
+        useEntityContext: useProject,
+        onSuccess,
     });
-
-    const { reset } = form;
-
-    const onSubmit = async (data: Project) => {
-        try {
-            if (project?.id) {
-                const isEditSuccess = await editEntity(project.id, data);
-                if (isEditSuccess && onSuccess) {
-                    onSuccess();
-                    reset();
-                    fireToast({
-                        title: 'Edit successful',
-                        variant: 'success',
-                    });
-                }
-            }
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                fireToast({
-                    title: error.message,
-                    variant: 'destructive',
-                });
-            }
-        }
-    };
-
-    return {
-        form,
-        isLoading,
-        onSubmit
-    };
 };
 
 export default useEditProject;
