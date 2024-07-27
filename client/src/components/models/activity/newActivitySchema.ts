28433507ab20f4136474507631bf4b6065c6a750
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-07-28 00:07:22 +0300

    fixed calendar date issue

--- client/src/components/models/activity/newActivitySchema.ts
+++ client/src/components/models/activity/newActivitySchema.ts
@@ -11,8 +11,8 @@ export const newActivitySchema = z.object({
     name: z.string().min(3, {
         message: 'Activity name must be at least 3 characters.'
     }).max(50),
-    dateFrom: z.coerce.date().transform((date) => format(date, 'yyyy-MM-dd')),
-    dateTo: z.coerce.date().transform((date) => format(date, 'yyyy-MM-dd')),
+    dateFrom: z.coerce.date().transform((date) => format(date, 'yyyy-MM-dd')).optional(),
+    dateTo: z.coerce.date().transform((date) => format(date, 'yyyy-MM-dd')).optional(),
     status: z.nativeEnum(ActivityStatus, {
         message: 'Please select status.'
     }),
--- client/src/types/activity-types/activityTypes.ts
+++ client/src/types/activity-types/activityTypes.ts
@@ -1,8 +1,8 @@
 export type Activity = {
     id?: number;
     name: string;
-    dateFrom: string,
-    dateTo: string,
+    dateFrom?: string,
+    dateTo?: string,
     status: 'active' | 'inactive';
 }
 export interface ActivityFormProps {
