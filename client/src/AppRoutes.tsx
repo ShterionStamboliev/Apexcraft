d4e2beaae3e18e1bf96679267302b557d5f8ffff
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-10-07 22:48:48 +0300

    implemented role based guards

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LayoutHomepage from './components/layouts/Home/LayoutHomepage';
 import UserLoginForm from './components/forms/user-form/UserFormLogin/UserLoginForm';
 import LayoutLoginForm from './components/layouts/Login/LayoutLoginForm';
-import RouteGuard from './components/guards/RouteGuard';
 import LayoutUsersTable from './components/layouts/Table/LayoutUsersTable';
 import UsersTablePage from './components/pages/UsersTablePage';
 import Homepage from './components/pages/Homepage';
@@ -13,75 +12,78 @@ import CompaniesTablePage from './components/pages/CompaniesTablePage';
 import ArtisansTablePage from './components/pages/ArtisansTablePage';
 import ProjectTasksPage from './components/pages/ProjectTasksPage';
 import TaskEditPage from './components/pages/TaskEditPage';
+import RouteGuard from './components/guards/RouteGuard';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            {/* <Route element={<RouteGuard />}> */}
-            <Route
-                path='/users'
-                element={
-                    <LayoutUsersTable>
-                        <UsersTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/artisans'
-                element={
-                    <LayoutUsersTable>
-                        <ArtisansTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/companies'
-                element={
-                    <LayoutUsersTable>
-                        <CompaniesTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/activities'
-                element={
-                    <LayoutUsersTable>
-                        <ActivitiesTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/measures'
-                element={
-                    <LayoutUsersTable>
-                        <MeasuresTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/projects'
-                element={
-                    <LayoutUsersTable>
-                        <ProjectsTablePage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/projects/:id/tasks'
-                element={
-                    <LayoutUsersTable>
-                        <ProjectTasksPage />
-                    </LayoutUsersTable>
-                }
-            />
-            <Route
-                path='/projects/:id/tasks/:taskId/edit'
-                element={
-                    <LayoutUsersTable>
-                        <TaskEditPage />
-                    </LayoutUsersTable>
-                }
-            />
+            <Route element={<RouteGuard allowedRoles={['manager']} />}>
+                <Route
+                    path='/users'
+                    element={
+                        <LayoutUsersTable>
+                            <UsersTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/artisans'
+                    element={
+                        <LayoutUsersTable>
+                            <ArtisansTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/companies'
+                    element={
+                        <LayoutUsersTable>
+                            <CompaniesTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/activities'
+                    element={
+                        <LayoutUsersTable>
+                            <ActivitiesTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/measures'
+                    element={
+                        <LayoutUsersTable>
+                            <MeasuresTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/projects'
+                    element={
+                        <LayoutUsersTable>
+                            <ProjectsTablePage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/projects/:id/tasks'
+                    element={
+                        <LayoutUsersTable>
+                            <ProjectTasksPage />
+                        </LayoutUsersTable>
+                    }
+                />
+                <Route
+                    path='/projects/:id/tasks/:taskId/edit'
+                    element={
+                        <LayoutUsersTable>
+                            <TaskEditPage />
+                        </LayoutUsersTable>
+                    }
+                />
+            </Route>
+            
             <Route
                 path='/'
                 element={
@@ -90,8 +92,6 @@ const AppRoutes = () => {
                     </LayoutHomepage>
                 }
             />
-            {/* </Route> */}
-
             <Route
                 path='/login'
                 element={
@@ -100,7 +100,6 @@ const AppRoutes = () => {
                     </LayoutLoginForm>
                 }
             />
-
             <Route
                 path='*'
                 element={
@@ -108,6 +107,7 @@ const AppRoutes = () => {
                 }
             />
         </Routes>
+
     )
 }
 
