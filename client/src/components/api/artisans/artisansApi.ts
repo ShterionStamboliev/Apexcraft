2b649ec0b931a45807b29d9333647c4ad04aee36
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-04 17:15:05 +0200

    cleaned artisans api

--- client/src/components/api/artisans/artisansApi.ts
+++ client/src/components/api/artisans/artisansApi.ts
@@ -1,4 +1,4 @@
-import { Artisan, PaginatedArtisans } from '@/types/artisan-types/artisanTypes';
+import { Artisan } from '@/types/artisan-types/artisanTypes';
 import { apiCall } from '../apiCall';
 
 const useArtisansApi = () => {
@@ -7,17 +7,11 @@ const useArtisansApi = () => {
         return data;
     };
 
-    const getPaginatedArtisans = async (page: number, limit: number): Promise<PaginatedArtisans> => {
-        const data: PaginatedArtisans = await apiCall(`/artisans?_page=${page}&_limit=${limit}`, 'GET');
-        return data;
-    };
-
     const editArtisan = async (artisanId: string, artisanData: Artisan): Promise<void> => {
         return await apiCall(`/artisans/${artisanId}/edit`, 'PUT', artisanData);
     };
 
     return {
-        getPaginatedArtisans,
         createArtisan,
         editArtisan
     }
