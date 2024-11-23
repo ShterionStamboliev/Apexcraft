a3c64a9ecd47292ca06c4839062e958d15d98a7e
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 22:47:01 +0200

    improved skeleton card

--- client/src/components/utils/SkeletonLoader/Projects/ProjectsSkeletonCard.tsx
+++ client/src/components/utils/SkeletonLoader/Projects/ProjectsSkeletonCard.tsx
@@ -11,26 +11,33 @@ const ProjectsSkeletonCard = ({ projects }: ProjectTypeProps) => {
 
     return (
         <>
-            {
-                Array.from({ length: projectsCardCount }).map((_, index) => (
-                    <Card className='w-full sm:w-[21rem]' key={index}>
-                        <CardHeader className='bg-header rounded-t-lg p-5'>
-                            <CardTitle>
-                                <Skeleton className='w-full h-[1rem]' />
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent className='space-y-1.5 p-5'>
-                            <Skeleton className='w-1/2 h-[1rem]' />
-                            <Skeleton className='w-1/2 h-[1rem]' />
-                            <Skeleton className='w-1/2 h-[1rem]' />
-                            <Skeleton className='w-1/2 h-[1rem]' />
-                        </CardContent>
-                        <CardFooter className='p-1.5 justify-center items-center rounded-b-lg border-t'>
-                            <Skeleton className='h-[2rem] w-[2rem]' />
-                        </CardFooter>
-                    </Card>
-                ))
-            }
+            <div className='flex flex-col border rounded-lg mt-8 mx-8 space-y-4 p-4 backdrop-blur-sm bg-slate-900/20'>
+                <Skeleton className='md:w-full lg:max-w-[12rem] h-9' />
+            </div>
+            <div className='flex flex-col border rounded-lg mb-24 mx-8 p-4 backdrop-blur-sm bg-slate-900/20'>
+                <div className='flex flex-wrap sm:w-full gap-4'>
+                    {
+                        Array.from({ length: projectsCardCount }).map((_, index) => (
+                            <Card className='w-full sm:w-[21rem]' key={index}>
+                                <CardHeader className='bg-header rounded-t-lg p-5'>
+                                    <CardTitle>
+                                        <Skeleton className='w-full h-[1rem]' />
+                                    </CardTitle>
+                                </CardHeader>
+                                <CardContent className='space-y-1.5 p-5'>
+                                    <Skeleton className='w-1/2 h-[1rem]' />
+                                    <Skeleton className='w-1/2 h-[1rem]' />
+                                    <Skeleton className='w-1/2 h-[1rem]' />
+                                    <Skeleton className='w-1/2 h-[1rem]' />
+                                </CardContent>
+                                <CardFooter className='p-1.5 justify-center items-center rounded-b-lg border-t'>
+                                    <Skeleton className='h-[2rem] w-[2rem]' />
+                                </CardFooter>
+                            </Card>
+                        ))
+                    }
+                </div>
+            </div>
         </>
     )
 }
