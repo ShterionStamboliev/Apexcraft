cf63c2f90391bd11ecd57d24aa4cffd9c392fd9e
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 11:48:26 +0200

    fixed users table skeleton loader

--- client/src/components/utils/SkeletonLoader/Users/UsersLoader.tsx
+++ client/src/components/utils/SkeletonLoader/Users/UsersLoader.tsx
@@ -11,16 +11,22 @@ const UsersLoader = ({ users }: UsersProps) => {
     const usersLength = users ? users.length : 10;
 
     return (
-        <Table className='w-full min-w-full'>
-            <UsersHeader />
-            <TableBody>
-                {
-                    Array.from({ length: usersLength }).map((_, i) => (
-                        <UsersSkeleton key={i} />
-                    ))
-                }
-            </TableBody>
-        </Table>
+        <div className="flex flex-col flex-1 py-8">
+            <div className='flex flex-col flex-1 py-8 items-center md:px-0'>
+                <div className='flex flex-col-reverse md:flex-col-reverse lg:flex-row gap-4 w-full mb-4 justify-center'>
+                    <Table className='w-full min-w-full'>
+                        <UsersHeader />
+                        <TableBody>
+                            {
+                                Array.from({ length: usersLength }).map((_, i) => (
+                                    <UsersSkeleton key={i} />
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+        </div>
     )
 }
 
