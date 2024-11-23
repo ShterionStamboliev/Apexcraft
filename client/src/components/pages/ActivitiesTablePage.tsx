895e1ca8ae1fe5be07d644bf170dd86addc18789
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 11:42:44 +0200

    replaced activities table with table body

--- client/src/components/pages/ActivitiesTablePage.tsx
+++ client/src/components/pages/ActivitiesTablePage.tsx
@@ -1,5 +1,5 @@
 import Sidebar from '../Sidebar/Sidebar';
-import ActivitiesTable from '../tables/ActivitiesTable/ActivitiesTable';
+import ActivitiesTableBody from '../tables/ActivitiesTable/ActivitiesTableBody';
 
 const ActivitiesTablePage = () => {
     return (
@@ -7,7 +7,7 @@ const ActivitiesTablePage = () => {
             <Sidebar />
 
             <div className="flex-1 flex px-2 md:gap-8">
-                <ActivitiesTable />
+                <ActivitiesTableBody />
             </div>
         </div>
     );
