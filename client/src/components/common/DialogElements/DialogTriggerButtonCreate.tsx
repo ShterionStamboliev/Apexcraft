6bafa32312f76c896b8a78098bc74d0596f61a6b
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 19:39:14 +0200

    improved dialog button styles

--- client/src/components/common/DialogElements/DialogTriggerButtonCreate.tsx
+++ client/src/components/common/DialogElements/DialogTriggerButtonCreate.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@/components/ui/button'
 import { DialogTrigger } from '@/components/ui/dialog'
+import { cn } from '@/lib/utils';
 import { Plus } from 'lucide-react'
 
 type DialogTriggerButtonCreateProps = {
     text: string;
+    className?: string;
 }
 
-const DialogTriggerButtonCreate = ({ text }: DialogTriggerButtonCreateProps) => {
+const DialogTriggerButtonCreate = ({ text, className }: DialogTriggerButtonCreateProps) => {
     return (
         <DialogTrigger asChild>
-            <Button className='w-full lg:max-w-[15rem]' variant="outline">
+            <Button className={cn('w-full lg:max-w-[15rem]', className)} variant="outline">
                 <Plus className="mr-2 h-4 w-4" />
                 <span className='font-bold'>{text}</span>
             </Button>
