c71186f6847eb400627e597b992c7e096e900b73
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 12:55:38 +0200

    added search query to artisans

--- server/controllers/artisans/getArtisansController.js
+++ server/controllers/artisans/getArtisansController.js
@@ -2,27 +2,57 @@ const pool = require("../../db");
 const { getControllerNameById } = require('../../utils/getControllerNameById');
 
 const getPaginatedArtisans = async (req, res) => {
-    const { _page = 1, _limit = 10 } = req.query;
+    const { _page = 1, _limit = 10, q = '' } = req.query;
+    const searchTerm = q ? `%${q}%` : null;
     const offset = (parseInt(_page) - 1) * parseInt(_limit);
 
     try {
-        const totalQuery = `SELECT COUNT(*) as total FROM tbl_artisans`;
-        const [[{ total }]] = await pool.query(totalQuery);
+        let totalQuery = `SELECT COUNT(*) as total FROM tbl_artisans`;
+        const totalQueryParams = [];
 
-        const query = `SELECT * FROM tbl_artisans LIMIT ${parseInt(_limit)} OFFSET ${offset}`;
+        if (q) {
+            totalQuery += ' WHERE name LIKE ?';
+            totalQueryParams.push(searchTerm);
+        }
 
-        const [rows] = await pool.query(query);
+        const [[{ total }]] = await pool.query(totalQuery, totalQueryParams);
+
+        let query = `
+            SELECT * 
+            FROM tbl_artisans
+        `;
+
+        const queryParams = [];
+
+        if (q) {
+            query += ' WHERE name LIKE ?';
+            queryParams.push(searchTerm);
+        }
+
+        query += ' LIMIT ? OFFSET ?';
+        queryParams.push(parseInt(_limit), offset);
+
+        const [rows] = await pool.query(query, queryParams);
 
         const artisansCompanyNames = await Promise.all(
             rows.map(async (artisan) => {
-                const companyName = await getControllerNameById(artisan.company_id, "tbl_companies", "name");
-                const userName = await getControllerNameById(artisan.user_id, "tbl_users", "name_and_family");
+                try {
+                    const companyName = await getControllerNameById(artisan.company_id, "tbl_companies", "name");
+                    const userName = await getControllerNameById(artisan.user_id, "tbl_users", "name_and_family");
 
-                return {
-                    ...artisan,
-                    company: companyName || null,
-                    artisanName: userName || null,
-                };
+                    return {
+                        ...artisan,
+                        company: companyName || null,
+                        artisanName: userName || null,
+                    };
+                } catch (error) {
+                    console.log('Error fetching artisan data', error);
+                    return {
+                        ...artisan,
+                        company: null,
+                        artisanName: null,
+                    }
+                }
             })
         );
 
