7eebb28680e590c372508544a92ef0257ef98b3b
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 19:40:24 +0200

    added new project table style

--- client/src/components/tables/ProjectsTable/ProjectsTableBody.tsx
+++ client/src/components/tables/ProjectsTable/ProjectsTableBody.tsx
@@ -5,6 +5,7 @@ import NoResultsFound from '@/components/common/FormMessages/NoResultsFound';
 import ProjectsCard from './ProjectsCard';
 import { useFetchDataQuery } from '@/components/hooks/custom-hooks/useQueryHook';
 import { Project } from '@/types/project-types/projectTypes';
+import CreateProject from '@/components/forms/projects-form/ProjectFormCreate/CreateProject';
 
 const ProjectsTableBody = () => {
     const { data: projects, isPending, isError } = useFetchDataQuery<Project[]>({
@@ -25,19 +26,26 @@ const ProjectsTableBody = () => {
 
     return (
         <>
-            {
-                projects.length === 0 ? (
-                    <NoResultsFound
-                        title='No projects found'
-                        description="It looks like you haven't added any projects yet."
-                        Icon={BrickWall}
-                    />
-                ) : (
-                    <ProjectsCard
-                        projects={projects}
-                    />
-                )
-            }
+            <div className='flex flex-col border rounded-lg mt-8 mx-8 space-y-4 p-4 backdrop-blur-sm bg-slate-900/20'>
+                <CreateProject />
+            </div>
+            <div className='flex flex-col border rounded-lg mt-8 mb-24 md:mt-0 mx-8 p-4 backdrop-blur-sm bg-slate-900/20'>
+                <div className='flex flex-wrap sm:w-full gap-4'>
+                    {
+                        projects.length === 0 ? (
+                            <NoResultsFound
+                                title='No projects found'
+                                description="It looks like you haven't added any projects yet."
+                                Icon={BrickWall}
+                            />
+                        ) : (
+                            <ProjectsCard
+                                projects={projects}
+                            />
+                        )
+                    }
+                </div>
+            </div>
         </>
     );
 };
