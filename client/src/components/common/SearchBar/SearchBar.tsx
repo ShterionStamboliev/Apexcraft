d85ad6883ef94d3e9ca29499660b6b34ee698d05
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 11:42:17 +0200

    improved search bar layout

--- client/src/components/common/SearchBar/SearchBar.tsx
+++ client/src/components/common/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
 
 type SearchBarProps = {
     search: string;
@@ -8,13 +9,16 @@ type SearchBarProps = {
 
 const SearchBar = ({ search, placeholder, handleSearch }: SearchBarProps) => {
     return (
-        <Input
-            type='text'
-            placeholder={placeholder}
-            value={search}
-            onChange={handleSearch}
-            className='mb-4 focus-visible:ring-blue-700 transition focus:ease-in-out duration-300 md:max-w-xs '
-        />
+        <div className='relative'>
+            <Search className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4' />
+            <Input
+                type='text'
+                placeholder={placeholder}
+                value={search}
+                onChange={handleSearch}
+                className='pr-10 pl-3 focus-visible:ring-blue-700 transition focus:ease-in-out duration-300 w-full md:w-full'
+            />
+        </div>
     )
 };
 
