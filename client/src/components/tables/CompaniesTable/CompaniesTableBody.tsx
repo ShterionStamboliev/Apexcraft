185088f606f012f45d4e22bcb96fdeec8da77872
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-23 12:17:45 +0200

    fixed tables layouts

--- client/src/components/tables/CompaniesTable/CompaniesTableBody.tsx
+++ client/src/components/tables/CompaniesTable/CompaniesTableBody.tsx
@@ -9,15 +9,21 @@ import { useGetPaginatedData } from '@/components/hooks/custom-hooks/useQueryHoo
 import { Company } from '@/types/company-types/companyTypes';
 import useSearchParamsHook from '@/components/hooks/custom-hooks/useSearchParamsHook';
 import Pagination from '@/components/common/Pagination/Pagination';
+import SearchBar from '@/components/common/SearchBar/SearchBar';
+import CreateCompany from '@/components/forms/companies-form/CompanyFormCreate/CreateCompany';
+import useSearchHandler from '@/components/hooks/custom-hooks/useSearchHandler';
 
 const CompaniesTableBody = () => {
     const { setSearchParams, itemsLimit, page } = useSearchParamsHook();
-    
+
+    const { search, handleSearch, debounceSearchTerm } = useSearchHandler({ setSearchParams });
+
     const { data: companies, isPending, isError } = useGetPaginatedData<Company>({
         URL: '/companies',
         queryKey: ['companies'],
         limit: itemsLimit,
-        page
+        page,
+        search: debounceSearchTerm
     });
 
     const totalPages: number | undefined = companies?.totalPages;
@@ -34,7 +40,15 @@ const CompaniesTableBody = () => {
     };
 
     return (
-        <>
+        <div className='flex flex-col flex-1 py-8 items-center md:px-0'>
+            <div className='flex flex-col-reverse md:flex-col-reverse lg:flex-row gap-4 w-full mb-4 md:w-2/3 justify-between'>
+                <SearchBar
+                    handleSearch={handleSearch}
+                    placeholder='Search companies...'
+                    search={search}
+                />
+                <CreateCompany />
+            </div>
             <Table className='w-full min-w-full'>
                 <CompaniesHeader />
                 <TableBody>
@@ -62,7 +76,7 @@ const CompaniesTableBody = () => {
                 setSearchParams={setSearchParams}
                 totalPages={totalPages}
             />
-        </>
+        </div>
     )
 }
 
--- client/src/components/tables/MeasuresTable/MeasuresTableBody.tsx
+++ client/src/components/tables/MeasuresTable/MeasuresTableBody.tsx
@@ -7,6 +7,7 @@ import MeasuresCard from './MeasuresCard';
 import MeasuresHeader from './MeasuresTableElements/MeasuresHeader/MeasuresHeader';
 import { useFetchDataQuery } from '@/components/hooks/custom-hooks/useQueryHook';
 import { Measure } from '@/types/measure-types/measureTypes';
+import CreateMeasure from '@/components/forms/measures-form/MeasureFormCreate/CreateMeasure';
 
 const MeasuresTableBody = () => {
     const { data: measures, isPending, isError } = useFetchDataQuery<Measure[]>({
@@ -26,28 +27,33 @@ const MeasuresTableBody = () => {
     };
 
     return (
-        <Table className='w-full min-w-full'>
-            <MeasuresHeader />
-            <TableBody>
-                {
-                    measures?.length === 0 ? (
-                        <TableRow>
-                            <TableCell colSpan={2} className='text-center text-3xl'>
-                                <NoResultsFound
-                                    title='No measures found'
-                                    description="It looks like you haven't added any measures yet."
-                                    Icon={Ruler}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    ) : (
-                        <MeasuresCard
-                            measures={measures}
-                        />
-                    )
-                }
-            </TableBody>
-        </Table>
+        <div className='flex w-full flex-col flex-1 py-8 items-center md:px-0'>
+            <div className='flex gap-4 items-end justify-end w-full mb-4 md:w-2/3'>
+                <CreateMeasure />
+            </div>
+            <Table className='w-full min-w-full'>
+                <MeasuresHeader />
+                <TableBody>
+                    {
+                        measures?.length === 0 ? (
+                            <TableRow>
+                                <TableCell colSpan={2} className='text-center text-3xl'>
+                                    <NoResultsFound
+                                        title='No measures found'
+                                        description="It looks like you haven't added any measures yet."
+                                        Icon={Ruler}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        ) : (
+                            <MeasuresCard
+                                measures={measures}
+                            />
+                        )
+                    }
+                </TableBody>
+            </Table>
+        </div>
     )
 }
 
