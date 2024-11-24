559db0469cd807c2970a1b4d7e519e0f676eda30
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-11-24 13:34:10 +0200

    table positioning fixes

--- client/src/components/pages/ProjectTasksPage.tsx
+++ client/src/components/pages/ProjectTasksPage.tsx
@@ -6,7 +6,7 @@ const ProjectTasksPage = () => {
         <div className="flex md:gap-60 min-h-screen">
             <Sidebar />
 
-            <div className="flex flex-col overflow-x-auto md:gap-8">
+            <div className="flex flex-col w-full overflow-x-auto md:gap-8">
                 <ProjectsTasksBody />
             </div>
         </div>
--- client/src/components/pages/ProjectsTablePage.tsx
+++ client/src/components/pages/ProjectsTablePage.tsx
@@ -6,7 +6,7 @@ const ProjectsTablePage = () => {
         <div className="flex md:gap-60 min-h-screen">
             <Sidebar />
 
-            <div className="flex flex-col overflow-x-auto md:gap-8">
+            <div className="flex flex-col w-full overflow-x-auto md:gap-8">
                 <ProjectsTableBody />
             </div>
         </div>
